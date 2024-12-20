7f48fca6820aaf0abbdee9f4a033c7d170211f15
Sam Valentino <sammyvalentino7@gmail.com>
2024-12-20 15:34:48 -0500

    completed js objects lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {
+    name: "Sam",
+    address: "7 Main St",
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value,
+    };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const { [key]: _, ...newEmployee } = employee;
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey (employee, key) {
+    delete employee[key];
+    return employee;
+}
